fe34d06794db81091df5f06150c57644a892fe95
Brittany Weaver <brittanyweaver13@gmail.com>
2023-04-05 07:58:40 -0400

    chap 5 completed

---
+++ src/main/java/org/BrittanyWeaver/Chap5/CodeMagnets/MultiFor.java
@@ -0,0 +1,16 @@
+package org.BrittanyWeaver.Chap5.CodeMagnets;
+
+public class MultiFor {
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 4; i++) {
+            for(int j = 4; j > 2; j--) {
+                System.out.println(i + " " + j);
+            }
+            if(i == 1) {
+               i++;
+           }
+        }
+    }
+}
