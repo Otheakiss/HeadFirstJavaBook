552ccab85346abdc63142b7cfc88bd6fc321c374
Brittany Weaver <brittanyweaver13@gmail.com>
2023-04-05 12:15:08 -0400

    chap 6 Sink a Startup Game

--- src/main/java/org/BrittanyWeaver/Chap6/Startup/Startup.java
+++ src/main/java/org/BrittanyWeaver/Chap6/Startup/Startup.java
@@ -5,9 +5,14 @@ import java.util.ArrayList;
 public class Startup {
 
     private ArrayList<String> locationCells;
+    private String name;
 
-    public void setLocationCells(ArrayList<String> locs) {
-        locationCells = locs;
+    public void setLocationCells(ArrayList<String> loc) {
+        locationCells = loc;
+    }
+
+    public void setName(String n) {
+        name = n;
     }
 
     public String checkYourself(String userInput) {
@@ -19,6 +24,7 @@ public class Startup {
 
             if(locationCells.isEmpty()) {
                 result = "kill";
+                System.out.println("Ouch! You sunk " + name + " :  ");
             } else {
                 result = "hit";
             }
