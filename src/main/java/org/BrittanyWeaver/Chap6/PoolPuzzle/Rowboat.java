f8f36880352002d71c39ab25f403d70a4ee5fd19
Brittany Weaver <brittanyweaver13@gmail.com>
2023-04-10 07:43:02 -0400

    chap 6 complete

---
+++ src/main/java/org/BrittanyWeaver/Chap6/PoolPuzzle/Rowboat.java
@@ -0,0 +1,8 @@
+package org.BrittanyWeaver.Chap6.PoolPuzzle;
+
+public class Rowboat extends Boat{
+
+    public void rowTheBoat() {
+        System.out.print("stroke natasha");
+    }
+}
---
+++ src/main/java/org/BrittanyWeaver/Chap6/PoolPuzzle/Sailboat.java
@@ -0,0 +1,8 @@
+package org.BrittanyWeaver.Chap6.PoolPuzzle;
+
+public class Sailboat extends Boat{
+
+    public void move() {
+        System.out.print("hoist sail ");
+    }
+}
